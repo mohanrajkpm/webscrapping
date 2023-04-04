d32142040602eab367ba60ae703608775ae2e350
mohanrajkpm <infomohanrajsoft@gmail.com>
2023-04-04 17:17:18 +0530

    Association orders and product

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :category
+  has_many :order_items
+  has_many :ordes, through: :order_items
 end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_329_091_520) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_113926) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'categories', force: :cascade do |t|
-    t.integer 'restaurant_id'
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['restaurant_id'], name: 'index_categories_on_restaurant_id'
+  create_table "categories", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_categories_on_restaurant_id"
   end
 
-  create_table 'items', force: :cascade do |t|
-    t.string 'title'
-    t.integer 'price'
-    t.text 'description'
-    t.integer 'category_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['category_id'], name: 'index_items_on_category_id'
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.integer "price"
+    t.text "description"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 100, null: false
+    t.integer "user_id", default: 1, null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
   end
 
-  create_table 'locations', force: :cascade do |t|
-    t.string 'city'
-    t.string 'state'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'locations_restaurants', force: :cascade do |t|
-    t.bigint 'location_id'
-    t.bigint 'restaurant_id'
-    t.index ['location_id'], name: 'index_locations_restaurants_on_location_id'
-    t.index ['restaurant_id'], name: 'index_locations_restaurants_on_restaurant_id'
+  create_table "locations_restaurants", force: :cascade do |t|
+    t.bigint "location_id"
+    t.bigint "restaurant_id"
+    t.index ["location_id"], name: "index_locations_restaurants_on_location_id"
+    t.index ["restaurant_id"], name: "index_locations_restaurants_on_restaurant_id"
   end
 
-  create_table 'restaurants', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
 end
